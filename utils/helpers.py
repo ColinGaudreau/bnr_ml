160246d88f9f5c9f00ac025d4af7e373cab1dc39
Colin Gaudreau <colingaudreau@marilyns-ipad.gateway.2wire.net>
2017-02-04 12:38:15 -0600

    Added full meshgrid funciton for theano

--- utils/helpers.py
+++ utils/helpers.py
@@ -38,6 +38,23 @@ def meshgrid2D(arr1, arr2):
 	arr1, arr2 = T.repeat(arr1, arr2.shape[0], axis=0), T.repeat(arr2, arr1.shape[1], axis=1)
 	return arr1, arr2
 
+def meshgrid(*xi):
+	assert(xi.__len__() > 1)
+	arrs = []
+	for i, x in enumerate(xi):
+		ds_args = ['x' if k != i else 0 for k in range(xi.__len__())]
+		arr = x.dimshuffle(*ds_args)
+
+		for j in range(xi.__len__()):
+			if j != i:
+				arr = T.repeat(arr, xi[j].shape[0], axis=j)
+		arrs.append(arr)
+
+	return arrs
+
+
+
+
 def softmax(mat, axis=1):
 	'''	
 	axis along which to take soft max, axis \in {0,1,2,3}
