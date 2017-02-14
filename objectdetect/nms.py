f5edeb2a12b641ba7918d9e8ee6f4e241a957d99
Colin Gaudreau <colingaudreau@wpa-5-1724.cc.umanitoba.ca>
2017-02-14 17:51:44 -0600

    Added scores to nms Viola Jones.

--- objectdetect/nms.py
+++ objectdetect/nms.py
@@ -8,10 +8,13 @@ def nms(boxes, *args, **kwargs):
 	box = copy.copy(boxes)
 	return _viola_jones(boxes, *args, **kwargs)
 
-def _viola_jones(boxes, overlap=0.4):
+def _viola_jones(boxes, scores=None, overlap=0.4):
 	'''
 	Non maximal suppression algorithm described in the Viola-Jones paper.
 	'''
+	if score is not None:
+		return _viola_jones_with_scores(boxes, scores, overlap=overlap)
+
 	regions = []
 	# split boxes into disjoint sets
 	while boxes.__len__() > 0:
@@ -37,3 +40,47 @@ def _viola_jones(boxes, overlap=0.4):
 			box_init = box_init + (box / len(region))
 		objs.append(box_init)
 	return objs
+
+def _viola_jones_with_scores(boxes, scores, overlap=0.4):
+	'''
+	Calculate score for the combined boxes
+	'''
+	assert(boxes.__len__() == scores.__len__())
+	regions = []
+	region_scores = []
+	# split boxes into disjoint sets
+	while boxes.__len__() > 0:
+		curr_box = boxes.pop(0)
+		curr_score = scores.pop(0)
+
+		in_region = False
+		for region, score in zip(regions, region_scores):
+			rlen = len(region)
+			for i in range(rlen):
+				box = region[i]
+				in_region |= box.iou(curr_box) > overlap
+				if in_region:
+					region.append(curr_box)
+					region_scores.append(curr_score)
+					break
+			if in_region:
+				break
+		if not in_region:
+			regions.append([curr_box])
+			region_scores.append([curr_score])
+
+	objs = []
+	new_scores = []
+	for region, score in zip(regions, region_scores):
+		box_init = region[0] / len(region)
+		score_init = score[0] / len(score)
+
+		for box, s in zip(region[1:], score[1:]):
+			box_init = box_init + (box / len(region))
+			box_init += (box / len(region))
+			score_init += (s / len(score))
+
+		objs.append(box_init)
+		new_scores.append(score_init)
+
+	return objs, new_scores
