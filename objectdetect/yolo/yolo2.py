1413c33b185c9e217e119b659d48dd00de08be60
Colin Gaudreau <colingaudreau@colins-mbp.gateway.mts.net>
2017-06-12 12:58:07 -0500

    changed extra_info to be JSON.

--- objectdetect/yolo/yolo2.py
+++ objectdetect/yolo/yolo2.py
@@ -208,11 +208,9 @@ class Yolo2ObjectDetector(BaseLearningObject):
 		for Xbatch, ybatch in gen_fn(train_annotations, **train_args):
 			ret_args = self._train_fn(Xbatch, ybatch, lambda_obj, lambda_noobj, lambda_anchor)
 			err = ret_args[0]
-			train_loss_batch.append(err)
-			extra = []
-			for e in ret_args[1:]:
-				extra.extend(e.tolist())
-			extras.append(extra)
+			extras['rows'].extend(ret_args[2].tolist())
+			extras['cols'].extend(ret_args[3].tolist())
+			extras['anchors'].extend(ret_args[4].tolist())
 			print_obj.println('Batch error: %.4f\n' % err)
 
 		for Xbatch, ybatch in gen_fn(test_annotations, **test_args):
