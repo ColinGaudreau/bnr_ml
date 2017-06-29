5c27eeec864608b7c58bbd050db42a3316bb1d89
Colin Gaudreau <colingaudreau@wpa-9-1014.cc.umanitoba.ca>
2017-06-29 15:30:44 -0500

    not sure

--- metrics/map.py
+++ metrics/map.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 
 import pdb
 
-def _ap_per_box(boxes, labels, min_iou=0.5, return_pr_curve=False):
+def _confusion_per_box(boxes, labels, min_iou=0.5, return_pr_curve=False):
 	'''
 	Calculate average precision given predictions, labels, class.
 
@@ -43,7 +43,54 @@ def _ap_per_box(boxes, labels, min_iou=0.5, return_pr_curve=False):
 	# tp, fp = np.cumsum(tp), np.cumsum(fp)
 	# recall = tp / num_labels if num_labels > 0 else tp * 0
 	# precision = tp / (tp + fp)
+
 	return scores[idx], tp, fp, num_labels
+
+def confusion(detector, annotations, num_to_label, verbose=True, print_obj=StreamPrinter(open('/dev/stdout', 'w')), loc='', detector_args={}):
+	confusion_data = {}
+
+	for _, label in num_to_label.iteritems():
+		confusion_data[label] = {'scores': [], 'tp': [], 'fp': [], 'n_labels': []}
+	detector_args.update({'num_to_label': num_to_label})
+	
+	if verbose:
+		print_obj.println('Doing confusion matrix calculation...')
+	for i, annotation in enumerate(annotations):
+		labels = []
+		# get all object types in image
+		img_classes = np.unique([o['label'] for o in annotation['annotations']]).tolist()
+
+		boxes = detector.detect(imread(loc + annotation['image']), **detector_args)
+		for cls in img_classes:
+			labels = [label for label in annotation['annotations'] if label['label'] == cls]
+
+			in_pred = False
+			for box in boxes:
+				if box.cls == cls:
+					in_pred = True
+					break
+
+			if in_pred:
+				scores, tp, fp, n_labels = _confusion_per_box([box for box in boxes if box.cls == cls], labels)
+				confusion_data[cls]['scores'].extend(scores.tolist())
+				confusion_data[cls]['tp'].extend(tp.tolist())
+				confusion_data[cls]['fp'].extend(fp.tolist())
+				confusion_data[cls]['n_labels'].append(n_labels)
+
+		if verbose:
+			print_obj.flush()
+			print_obj.write('\rAnnotation %d/%d' % (i+1,len(annotations)))
+
+	confusion = {}
+	for key, data in confusion_data.iteritems():
+		scores, tp, fp, n_labels = np.asarray(data['scores']), np.asarray(data['tp']), np.asarray(data['fp']), np.sum(data['n_labels'])
+		idx = np.argsort(scores)[::-1]
+		tp, fp = tp[idx], fp[idx]
+		tp, fp = np.cumsum(tp), np.cumsum(fp)
+		tn = np.maximum(0., n_labels - tp)
+		confusion[key] = {'tp': tp, 'fp': fp, 'fn': fn}
+
+	return confusion
 
 def _ap(precision, recall):
 	prec, rec = np.zeros(precision.size + 2), np.zeros(recall.size + 2)
@@ -55,120 +102,85 @@ def _ap(precision, recall):
 	index = np.asarray([i + 1 for i in range(rec.size - 1) if np.abs(rec[i] - rec[i+1]) > 1e-5])
 	return ((rec[index] - rec[index - 1]) * prec[index]).sum(), prec, rec
 
-def map(detector, annotations, num_to_label, verbose=True, print_obj=StreamPrinter(open('/dev/stdout', 'w')), loc='', detector_args={}):
-	aps = {}
-	for _, label in num_to_label.iteritems():
-		aps[label] = {'scores': [], 'tp': [], 'fp': [], 'n_labels': []}
-	detector_args.update({'num_to_label': num_to_label})
-	
+def map(detector, annotations, num_to_label, verbose=True, print_obj=StreamPrinter(open('/dev/stdout', 'w')), loc='', detector_args={}):	
 	if verbose:
 		print_obj.println('Beginning mean average precision calculation...')
-	for i, annotation in enumerate(annotations):
-		labels = []
-		# get all object types in image
-		img_classes = np.unique([o['label'] for o in annotation['annotations']]).tolist()
 
-		preds = detector.detect(imread(loc + annotation['image']), **detector_args)
-		for cls in img_classes:
-			labels = [label for label in annotation['annotations'] if label['label'] == cls]
-
-			in_pred = False
-			for box in preds:
-				if box.cls == cls:
-					in_pred = True
-					break
-
-			if in_pred:
-				scores, tp, fp, n_labels = _ap_per_box([box for box in preds if box.cls == cls], labels)
-				aps[cls]['scores'].extend(scores.tolist())
-				aps[cls]['tp'].extend(tp.tolist())
-				aps[cls]['fp'].extend(fp.tolist())
-				aps[cls]['n_labels'].append(n_labels)
-			# else:
-				# aps[cls]['ap'].append(0.)
-
-		if verbose:
-			# mean_ap = np.mean([np.mean(ap['ap']) for _, ap in aps.iteritems() if len(ap['ap']) > 0])
-			print_obj.flush()
-			print_obj.write('\rAnnotation %d/%d' % (i+1,len(annotations)))
-			# print_obj.write('\rAnnotation %d complete, mAP so far: %.3f' % (i, mean_ap))
-
-	if verbose:
-		print_obj.println('')
+	confusion = confusion(detector, annotations, num_to_label, verbose=verbose, print_obj=print_obj, loc=loc, detector_args=detector_args)
 
 	class_ap = {}
-	for key, ap in aps.iteritems():
-		scores, tp, fp, n_labels = np.asarray(ap['scores']), np.asarray(ap['tp']), np.asarray(ap['fp']), np.sum(ap['n_labels'])
-		idx = np.argsort(scores)[::-1]
-		tp, fp = tp[idx], fp[idx]
-		tp, fp = np.cumsum(tp), np.cumsum(fp)
-		recall = tp / n_labels if n_labels > 0 else np.zeros_like(tp)
+	for key, conf in confusion.iteritems():
+		tp, fp, fn = conf['tp'], conf['fp'], conf['fn']
+
+		recall = np.nan_to_num(tp / (tp + fn), 0.)
 		precision = tp / (tp + fp)
+
 		a, precision, recall = _ap(precision, recall)
+
 		class_ap[key] = {'ap': a, 'precision': precision, 'recall': recall}
 	return class_ap
 
-def _precision_per_box(boxes, labels, min_iou=0.5, return_pr_curve=False):
-	'''
-	Calculate average precision given predictions, labels, class.
+# def _precision_per_box(boxes, labels, min_iou=0.5, return_pr_curve=False):
+# 	'''
+# 	Calculate average precision given predictions, labels, class.
 
-	labels - list of dictionaries containing labels, this should be restricted to
-		the relevant class.
-	'''
-	boxes, scores = np.asarray(boxes), np.asarray([box.confidence for box in boxes])
+# 	labels - list of dictionaries containing labels, this should be restricted to
+# 		the relevant class.
+# 	'''
+# 	boxes, scores = np.asarray(boxes), np.asarray([box.confidence for box in boxes])
 
-	# order predictions in descending order of confidence
-	idx = np.argsort(scores)[::-1]
-	boxes = boxes[idx]
-	num_labels = labels.__len__()
-	was_used = np.zeros(num_labels, dtype=np.bool)
-	tp, fp = 0., 0.
-	classes = np.unique([box.cls for box in boxes])
+# 	# order predictions in descending order of confidence
+# 	idx = np.argsort(scores)[::-1]
+# 	boxes = boxes[idx]
+# 	num_labels = labels.__len__()
+# 	was_used = np.zeros(num_labels, dtype=np.bool)
+# 	tp, fp = 0., 0.
+# 	classes = np.unique([box.cls for box in boxes])
 
-	for i in range(boxes.size):
-		box = boxes[i]
-		best_iou = 0.
-		best_label = -1
-		for j, label in enumerate(labels):
-			gt_box = BoundingBox(label['x'], label['y'], label['x'] + label['w'], label['y'] + label['w'])
-			if label['label'] == box.cls and box.iou(gt_box) > best_iou:
-				best_iou = box.iou(gt_box)
-				best_label = j
+# 	for i in range(boxes.size):
+# 		box = boxes[i]
+# 		best_iou = 0.
+# 		best_label = -1
+# 		for j, label in enumerate(labels):
+# 			gt_box = BoundingBox(label['x'], label['y'], label['x'] + label['w'], label['y'] + label['w'])
+# 			if label['label'] == box.cls and box.iou(gt_box) > best_iou:
+# 				best_iou = box.iou(gt_box)
+# 				best_label = j
 
-		if best_iou > min_iou:
-			if not was_used[best_label]:
-				tp += 1.
-			else:
-				fp += 1.
-			was_used[best_label] = True
-		else:
-			fp += 1.
+# 		if best_iou > min_iou:
+# 			if not was_used[best_label]:
+# 				tp += 1.
+# 			else:
+# 				fp += 1.
+# 			was_used[best_label] = True
+# 		else:
+# 			fp += 1.
 
-	return tp / (tp + fp)
+# 	return tp / (tp + fp)
 
-def precision(detector, annotations, num_to_label, verbose=True, print_obj=StreamPrinter(open('/dev/stdout', 'w')), detector_args={}):
-	precisions = []
-	detector_args.update({'num_to_label': num_to_label})
+# def precision(detector, annotations, num_to_label, verbose=True, print_obj=StreamPrinter(open('/dev/stdout', 'w')), detector_args={}):
+# 	precisions = []
+# 	detector_args.update({'num_to_label': num_to_label})
 	
-	if verbose:
-		print_obj.println('Beginning precision calculation...')
-	for i, annotation in enumerate(annotations):
-		labels = []
-		# get all object types in image
-		img_classes = np.unique([o['label'] for o in annotation['annotations']]).tolist()
+# 	if verbose:
+# 		print_obj.println('Beginning precision calculation...')
+# 	for i, annotation in enumerate(annotations):
+# 		labels = []
+# 		# get all object types in image
+# 		img_classes = np.unique([o['label'] for o in annotation['annotations']]).tolist()
 
-		boxes = detector.detect(imread(annotation['image']), **detector_args)
+# 		boxes = detector.detect(imread(annotation['image']), **detector_args)
 
-		if len(boxes) == 0:
-			precisions.append(0.)
-		else:
-			precisions.append(_precision_per_box(boxes, annotation['annotations']))
+# 		if len(boxes) == 0:
+# 			precisions.append(0.)
+# 		else:
+# 			precisions.append(_precision_per_box(boxes, annotation['annotations']))
 
-		if verbose:
-			mean_precision = np.mean(precisions)
-			print_obj.println('Annotation %d complete, mean precision so far: %.3f' % (i, mean_precision))
+# 		if verbose:
+# 			mean_precision = np.mean(precisions)
+# 			print_obj.println('Annotation %d complete, mean precision so far: %.3f' % (i, mean_precision))
 
-	return np.mean(precisions)
+# 	return np.mean(precisions)
 
 def _f1_per_box(boxes, labels, beta=1., min_iou=0.5, return_pr_curve=False):
 	'''
@@ -208,29 +220,21 @@ def _f1_per_box(boxes, labels, beta=1., min_iou=0.5, return_pr_curve=False):
 
 	return (1 + beta**2)*tp / ((1+beta**2)*tp + fp + (beta**2)*max(len(labels) - tp, 0))
 
-def f1_score(detector, annotations, num_to_label, beta=1., min_iou=0.5, verbose=True, print_obj=StreamPrinter(open('/dev/stdout', 'w')), loc='', detector_args={}):
-	f1_scores = []
-	detector_args.update({'num_to_label': num_to_label})
-	
+def f1_score(detector, annotations, num_to_label, beta=1., min_iou=0.5, verbose=True, print_obj=StreamPrinter(open('/dev/stdout', 'w')), loc='', detector_args={}):	
 	if verbose:
 		print_obj.println('Beginning f1 score calculation...')
-	for i, annotation in enumerate(annotations):
-		labels = []
-		# get all object types in image
-		img_classes = np.unique([o['label'] for o in annotation['annotations']]).tolist()
 
-		boxes = detector.detect(imread(loc + annotation['image']), **detector_args)
+	confusion = confusion(detector, annotations, num_to_label, verbose=verbose, print_obj=print_obj, loc=loc, detector_args=detector_args)
 
-		if len(boxes) == 0:
-			f1_scores.append(0.)
-		else:
-			f1_scores.append(_f1_per_box(boxes, annotation['annotations'], beta=beta, min_iou=min_iou))
+	f1_scores = {}
+	for cls, conf in confusion.iteritems():
+		tp, fp, fn = conf['tp'][-1], conf['fp'][-1], conf['fn'][-1]
 
-		if verbose:
-			print_obj.flush()
-			print_obj.write('\rAnnotation %d complete, mean f1 score so far: %.3f' % (i, np.mean(f1_scores)))
+		f1 = (1 + beta**2) * tp / ((1 + beta**2) * tp + fp + (beta**2) * fn)
 
-	return np.mean(f1_scores)
+		f1_scores[cls] = f1
+
+	return f1_scores
 
 
 		
